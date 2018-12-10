a983c1082be06b71de93a3c7aa922daed3d3c930
Kfir Awad <kfir40mailminer@gmail.com>
2018-12-10 09:20:46 +0200

    programming

--- app/src/main/java/com/leo/cyber2021ex02/MainActivity.java
+++ app/src/main/java/com/leo/cyber2021ex02/MainActivity.java
@@ -2,12 +2,23 @@ package com.leo.cyber2021ex02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private int clicks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        clicks = 0;
+    }
+
+    public void onButtonClick(View view) {
+        button.setText("Click number: " + ++clicks);
     }
 }
